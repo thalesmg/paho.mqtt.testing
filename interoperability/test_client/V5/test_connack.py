378da45adcdf4b1fea1f1bfbb8592cfc03366a0e
zhanghongtong <zhanghongtong@foxmail.com>
2019-09-29 17:19:05 +0800

    skip test cases

--- interoperability/test_client/V5/test_connack.py
+++ interoperability/test_client/V5/test_connack.py
@@ -77,6 +77,7 @@ def test_assigned_cliend_id():
   assert hasattr(connack.properties, "AssignedClientIdentifier") and connack.properties.AssignedClientIdentifier != ''
   client.disconnect()
 
+@pytest.mark.skip("This is a bug")
 def test_maximum_packet_size():
   # [MQTT-3.2.2-15]
   # 1. server max packet size
